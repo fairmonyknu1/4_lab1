a6364218363609028e433a749412246269d3e5af
Fairmont77 <4ydovvans@gmail.com>
2024-11-10 11:14:08 +0200

    add unit test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+# products/tests.py
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Product
+
+class ProductModelTest(TestCase):
+    def setUp(self):
+        Product.objects.create(name="Test Product", price=10.00)
+
+    def test_product_name(self):
+        product = Product.objects.get(name="Test Product")
+        self.assertEqual(product.name, "Test Product")
